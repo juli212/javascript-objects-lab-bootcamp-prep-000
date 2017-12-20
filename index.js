879c8e9a9c789392be7ef13ba9e76276b0f65433
Julia Giraldi <jjgiraldi@gmail.com>
2017-12-20 16:26:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
-  delete newObject.key
-  return object
+  delete newObject[key]
+  return newObject
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
