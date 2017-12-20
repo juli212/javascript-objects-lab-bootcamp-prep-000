7c911259cd180e9acc4ab34bf2ceefc1eb612062
Julia Giraldi <jjgiraldi@gmail.com>
2017-12-20 15:54:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromOjbectByKey(object, key) {
+  
+  return object
+}
+
+function destructivelyDeleteFromOjbectByKey(object, key) {
+  
+  return object
+}
